cc9cb0471d7ad704baa889565d33fd2cf1ad9e30
David Nöggerath <dnoeggerath@online.de>
2012-01-25 15:00:41 +0100

    Updatet Modelling of Manifest and Activities

--- src/UML2Android/Utility.java
+++ src/UML2Android/Utility.java
@@ -1,12 +1,49 @@
 package UML2Android;
 
-import org.eclipse.uml2.uml.Property;
-import org.eclipse.uml2.uml.Stereotype;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Utility {
+	/**
+	 * Formats a file-path to a package-name
+	 */
+	public String formatPackageName(String name) {
+		return name.replace("/", ".");
+	}
 	
-	public double divide(Double r1, Double r2) {
-		return r1 / r2;
+	/**
+	 * Copies a File
+	 */
+	public boolean copyImageFile(String sourcePath, String destinationPath, String filename) {
+		File oldFile = new File(sourcePath, filename);
+		File newFile = new File(destinationPath);
+		
+		if(!newFile.exists()) {
+			newFile.mkdirs();
+		}
+		
+		try {
+			InputStream in = new FileInputStream(oldFile);
+			OutputStream out = new FileOutputStream(new File(newFile, "ic_launcher.png"));
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			in.close();
+			out.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 }
